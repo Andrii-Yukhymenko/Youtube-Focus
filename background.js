04546f0ce967c4114f48b262a2fd4733b2d5b5c4
Andrii Yukhymenko <andriy111224@gmail.com>
2021-09-04 23:37:51 +0300

    Добавил запись в localstorage

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
 let button = document.querySelector(".button");
-
+//todo обдумать когда какое значение state переключать
 button.addEventListener("click", () => {
-  if (button.classList.contains("active")) {
+  if (
+    localStorage.getItem("state") === "on" ||
+    localStorage.getItem("state") === null
+  ) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       let activeTab = tabs[0];
       chrome.tabs.sendMessage(activeTab.id, { message: "turn-off" });
       button.classList.remove("active");
       button.innerText = "Включить";
+      localStorage.setItem("state", "off");
     });
   } else {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -14,6 +18,7 @@ button.addEventListener("click", () => {
       chrome.tabs.sendMessage(activeTab.id, { message: "turn-on" });
       button.classList.add("active");
       button.innerText = "Выключить";
+      localStorage.setItem("state", "on");
     });
   }
 });
